f661971805b7b9cf08f591e3491ed73b3d1559c3
Nejc Kejzar <kejzar.n@gmail.com>
2024-01-24 22:02:53 +0000

    Init particles.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,16 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+document.addEventListener(
+	"DOMContentLoaded",
+	(event) => {
+	  particlesJS.load("particles-js", "assets/js/particles.json", function () {
+		console.log("particles.js loaded - callback");
+	  });
+	},
+	false
+	);  
+
 (function($) {
 
 	skel
